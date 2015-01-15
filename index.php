7f1d607690dee12185455ec2c230df318e04a1b5
Francisco Miranda <francisco.raul.mg@gmail.com>
2015-01-14 22:07:19 -0500

    texto1

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
  <head>
    <title>Venta por Referencia</title>
 
-   <meta charset="iso-8859-1">
+   <meta charset="utf-8">
    <meta http-equiv="X-UA-Compatible" content="IE=edge">
    <meta name="viewport" content="width=device-width, initial-scale=1">
    <meta name="author" content="Grupo 6">
